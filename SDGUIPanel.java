5fcce6f7dfe3bb21248b65e1b60eaccab708380a
ripetomatoes <ripetomatoes@outlook.com>
2015-02-14 23:55:40 -0500

    good gui stuff

--- SDGUIPanel.java
+++ SDGUIPanel.java
@@ -1,24 +1,60 @@
 //Sam Wang
 import java.io.*;
+
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
 import java.util.*;
+
 import javax.sound.sampled.AudioSystem;
+
 import java.applet.*;
 
 public class SDGUIPanel extends JPanel{
 //LOAD/INITIALIZE.=======================================================================
 	private Font guifont;
 	private Font guifontcalibri;
-	private char selected;
+	private String selectedletter;
+	private int selectednumber;
+
+	private int prevnumber=-1;
+	
+	private Image[] signlist={new ImageIcon("SpiqueDataGUIAssets/Image/A.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/A.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/B.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/C.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/D.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/E.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/F.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/G.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/H.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/I.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/J.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/K.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/L.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/M.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/N.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/O.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/P.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/Q.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/R.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/S.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/T.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/U.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/V.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/W.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/X.png").getImage(),	
+			new ImageIcon("SpiqueDataGUIAssets/Image/Y.png").getImage(),
+			new ImageIcon("SpiqueDataGUIAssets/Image/Z.png").getImage()};
+	
 	private String[][] datatest={{"tmx","tmy","tmz","tpx","tpy","tpz","tix","tiy","tiz","tdx","tdy","tdz"},
     		{"imx","imy","imz","ipx","ipy","ipz","iix","iiy","iiz","idx","idy","idz"},
     		{"mmx","mmy","mmz","mpx","mpy","mpz","mix","miy","miz","mdx","mdy","mdz"},
     		{"rmx","rmy","rmz","rpx","rpy","rpz","rix","riy","riz","rdx","rdy","rdz"},
     		{"pmx","pmy","pmz","ppx","ppy","ppz","pix","piy","piz","pdx","pdy","pdz"}};
-
+	private String[] alphabet={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
 //=======================================================================================
 	public SDGUIPanel(){
 		super();
@@ -26,21 +62,31 @@ public class SDGUIPanel extends JPanel{
 		grabFocus();
 		loadFont("GlennsHand.ttf");
 		guifontcalibri=new Font("calibri",Font.PLAIN,24);
-		
 		Random die=new Random();
-		selectedletter=(char)(die.nextInt(26) + "A");
+		selectednumber=die.nextInt(26);
+		selectedletter=alphabet[selectednumber];
+		//System.out.println(selectedletter);
 	}
 	//GETDATA METHODS.=================================================================	
 	//public void getData()
 	
 	
-	//METHODS.=========================================================================		
+	//METHODS.=========================================================================
+	public void changeLetter(){
+		Random die=new Random();
+		while (selectednumber==prevnumber){
+			selectednumber=die.nextInt(26);
+		}
+		prevnumber=0+selectednumber;
+		selectedletter=alphabet[selectednumber];
+	}
+	
 	public void loadFont(String fontname){ //Loads a custom font to use for the gui.
 		String fontpath="SpiqueDataGUIAssets/Font/"+fontname;
 		try{
 	    	InputStream is=SpiqueDataGUI.class.getResourceAsStream(fontpath);
 	    	guifont=Font.createFont(Font.TRUETYPE_FONT,is);
-	    	guifont=guifont.deriveFont(144f); //Changes loaded font size.
+	    	guifont=guifont.deriveFont(172f); //Changes loaded font size.
 	    }
 	    catch (Exception ex){ //Loads a common system font if the custom font cannot be loaded.
 	    	System.err.println("Font was not found in "+fontpath+". Using basic arial font.");
@@ -48,18 +94,37 @@ public class SDGUIPanel extends JPanel{
 	    }
 	}
 	//DRAW METHODS.======================================================================
-	public void drawLetter(Graphics g,char selectedletter){
+	public void drawLetter(Graphics g,String select){
 		Graphics2D g2d=(Graphics2D)g.create();
 		g2d.setFont(guifont);
-		g2d.setColor(255,255,255);
-		String drawme=selectedletter;
-        g2d.drawString(drawme,174,50);
+		g2d.setColor(new Color(56,200,53));
+		
+        int stringwidth=(int)g2d.getFontMetrics().getStringBounds(select,g2d).getWidth();
+        int stringheight=(int)g2d.getFontMetrics().getStringBounds(select,g2d).getHeight();
+        
+        g2d.setColor(new Color(3,5,6)); //For shadow.
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,0.5f));
+        
+        //g2d.drawString(select,(512-stringwidth/2)+5,205); //Score shadow, for aesthetics.
+        g2d.drawString(select,105,288-(stringheight/2)+5);
+        
+        g2d.setColor(new Color(255,255,255));
+        g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER,1.0f));
+        
+        //g2d.drawString(select,512-stringwidth/2,200);
+        g2d.drawString(select,100,288-(stringheight/2));
+        
+        AffineTransform at=new AffineTransform();
+		at.translate(512-signlist[selectednumber].getWidth(null)/2,288-signlist[selectednumber].getHeight(null)/2);
+        g2d.drawImage(signlist[selectednumber],at,this);
+        
+        
         g2d.dispose();
 	}
 	
 	public void drawTitle(Graphics g){
 		Graphics2D g2d=(Graphics2D)g.create();
-		g2d.setFont(guifont);
+		g2d.setFont(guifontcalibri);
         g2d.setColor(new Color(0,176,34));
         g2d.drawString("Thumb",174,50);
         g2d.drawString("Index",348,50);
@@ -110,12 +175,18 @@ public class SDGUIPanel extends JPanel{
         }
 		g2d.dispose();
 	}
+//=======================================================================================
+	public String getLetter(){
+		return selectedletter;
+	}
+	
 //Paint Component.=======================================================================
 	public void paintComponent(Graphics g){
-	 	g.setColor(Color.white);
-	 	g.fillRect(0,0,1024,768);
-	 	drawLetter(g,selectedletter);
-	 	//drawTitle(g);
-	 	//drawData(g,datatest);
+		g.setColor(Color.white);
+	 	//g.setColor(new Color(42,132,153));
+	 	g.fillRect(0,0,1024,576);
+	 	//drawLetter(g,selectedletter);
+	 	drawTitle(g);
+	 	drawData(g,datatest);
 	}
 }
--- SpiqueDataGUI.java
+++ SpiqueDataGUI.java
@@ -1,6 +1,8 @@
 //Sam Wang
 import java.io.*;
+
 import javax.swing.*;
+
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
@@ -10,8 +12,10 @@ public class SpiqueDataGUI extends JFrame implements ActionListener{
  	SDGUIPanel gui;
 	javax.swing.Timer frameupdater=new javax.swing.Timer(1000/80,this);
 	
+	javax.swing.Timer testaction=new javax.swing.Timer(500,this);
+	
 	public SpiqueDataGUI(){
-		super("Spique Data GUI Utility");
+		super("Spique Interactive");
 		gui=new SDGUIPanel();
 		gui.setPreferredSize(new Dimension(1024,576));
 		gui.setLocation(0,0);
@@ -21,10 +25,17 @@ public class SpiqueDataGUI extends JFrame implements ActionListener{
 		setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
 		frameupdater.start();
+		
+		testaction.start();
 	}
 //ACTION PERFORMED.======================================================================
 	public void actionPerformed(ActionEvent evt){
 		gui.repaint(); //Repaint here as updating is disabled on pause.
+		
+		Object source=evt.getSource();
+		if(source==testaction){
+			gui.changeLetter();
+		}
 	}
 	
 	public static void main(String[]args){
